5d7b65714b268e4be3e6ced82f48923faf3decbc
friday <workFriday@protonmail.com>
2021-06-22 09:22:04 -0700

    feat: app base colors

---
+++ lib/app/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColorsDark {
+  Color brand = Color(0xffFF9608);
+  Color blueOcean = Color(0xff5AD0F5);
+  Color dark = Color(0xff353535);
+  Color tgBlue = Color(0xff76B3EB);
+  Color darkBlue = Color(0xff1664AC);
+  Color grey = Color(0xffE6E6E6);
+}
+
+// ignore: non_constant_identifier_names
+final AppColorsDark AppColors = new AppColorsDark();
